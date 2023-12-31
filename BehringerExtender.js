4429f9351ca8a8ea20d51d25d969bf535bab9abe
niklasberlin <github@niklas-aumueller.de>
2023-12-31 22:00:50 +0100

    further code cleanup
    removed more code not neccasary for the Behringer Extender, added first functions specific to Behringer Extender but those are untested

--- BehringerExtender.js
+++ BehringerExtender.js
@@ -54,32 +54,12 @@ function init()
         local.sendNoteOn(1,counter+0,local.values.strips.getChild('Strip '+(counter+1)).rec.get());
         //Init Select
         local.sendNoteOn(1,counter+24,local.values.strips.getChild('Strip '+(counter+1)).select.get());
-         //Calculate Top Scribble Strip Array
-        stripArray[counter]=local.values.strips.getChild('Strip '+(counter+1)).encoderName.get();
-        if ((stripArray[counter].length)>7){
-            stripArray[counter]=stripArray[counter].substring(0,7);
-        }else{
-            while((stripArray[counter].length)<7){
-                stripArray[counter]+=" ";
-            }
-        } 
     }
 
-    //Synchronize Arrays 8-15
-    for(counter=8;counter<16;counter++){
-       //Calculate Bottom Sysex
-        stripArray[counter]=local.values.strips.getChild('Strip '+(counter-7)).faderName.get();
-        if ((stripArray[counter].length)>7){
-            stripArray[counter]=stripArray[counter].substring(0,7);
-        }else{
-            while((stripArray[counter].length)<7){
-                stripArray[counter]+=" ";
-            }
-        } 
-    }
+    updateScribble();
 
     //Send Assembled String Array to scribble strips
-    local.sendSysex(0x00,0x00,0x66,0x14,0x12,0x00,stripArray);
+    //local.sendSysex(0x00,0x00,0x66,0x14,0x12,0x00,stripArray);
 
     //Calculate Clock Values
     UTCOffset = (yearSecs*1970) + (hourSecs*-2)+(minuteSecs*5) - 22;
@@ -98,6 +78,97 @@ function init()
     local.sendCC(1, 67, 48+Math.floor(Math.floor(seconds%10)));
     local.sendCC(1, 68, 48+Math.floor(Math.floor(seconds/10)));
 
+}
+
+
+//lets update the Text on the LCD: WORKS ONLY IN CTRL MODE
+//stripIndex: Integer, number of Strip 1..8
+//Text: String, Displayed Text, only 14 hars will be displayed, linebreak after 7 Chars
+//color: String, one of the following: "black", "red", "green", "yellow", "blue", "magenta", "cyan", "white"
+//invrt: Boolean, true if Text should be displayed inverted 
+//function updateScribble(stripIndex, Text, color , invert)
+//{
+//    script.log("Updating LCD Text for Strip "+stripIndex);
+//    var cc = 0;
+//    if (color === "black") {
+//        cc += 0;
+//    } else if (color === "red") {
+//        cc += 1;
+//    } else if (color === "green") {
+//        cc += 2;
+//    } else if (color === "yellow") {
+//        cc += 3;
+//    } else if (color === "blue") {
+//        cc += 4;
+//    } else if (color === "magenta") {
+//        cc += 5;
+//    } else if (color === "cyan") {
+//        cc += 6;
+//    } else if (color === "white") {
+//        cc += 7;
+//    }
+//    if (invert)
+//    {
+//        cc += 16; //invert upper half of LCD
+//        cc += 32; //invert lower half of LCD
+//    }
+//    if ((Text.length)>14){
+//        Text=Text.substring(0,14);
+//    }else{
+//        while((Text.length)<14){
+//            Text+=" ";
+//        }
+//    }
+//
+//    local.sendSysex(0xF0,0x00,0x20,0x32,0x15,0x4C, stripIndex-1, cc, Text,0xF7);
+//}
+
+function updateStripArray()
+{
+    for(counter=0;counter<8;counter++)
+    {
+        stripArray[counter]=local.values.strips.getChild('Strip '+(counter+1)).faderName.get();
+        if ((stripArray[counter].length)>14){
+            stripArray[counter]=stripArray[counter].substring(0,14);
+        }else{
+            while((stripArray[counter].length)<14){
+                stripArray[counter]+=" ";
+            }
+        }
+    }
+}
+
+
+//update scribbles in MC Mode
+function updateScribble()
+{
+    updateStripArray();
+    //send first row:
+    firstrow = "";
+    secondrow = "";
+    for(counter=0;counter<8;counter++)
+    {
+        if(stripArray[counter].length!=14){
+            script.log("Error: Text legth does not match Display resolution!");
+            return;
+        }
+        firstrow+=stripArray[counter].substring(0,7);
+        secondrow+=stripArray[counter].substring(7);
+    }
+    local.sendSysex(0x00, 0x00, 0x66, 0x15, 0x12, 0x00, firstrow);
+    local.sendSysex(0x00, 0x00, 0x66, 0x15, 0x12, 0x38, secondrow);
+}
+
+function updateScribleColor()
+{
+    colorArray = [];
+    for(counter=0;counter<8;counter++)
+    {
+        colorArray[counter] += parseInt(local.values.strips.getChild('Strip '+(counter+1)).displayColor.get());
+    }
+    script.log("ColorArray:");
+    script.log(colorArray);
+    local.sendSysex(0x00, 0x00, 0x66, 0x15, 0x72, colorArray, 1);
 }
 
 function update(deltaTime)
@@ -200,21 +271,6 @@ function moduleParameterChanged(param)
                 }
             }
         }
-
-        //Did we change the Views ?
-        if(param.name=="activeView"){
-            var i;
-            for(i=0;i<8;i++){
-                if(param.get()!=i){
-                    local.values.views.getChild(viewsArray[i]).set(0);
-                    local.sendNoteOn(1,i+62,0);
-                }
-                if(param.get()==i){
-                    local.values.views.getChild(viewsArray[i]).set(1);
-                    local.sendNoteOn(1,i+62,127);
-                }
-            }
-        }
     }
 }
 
@@ -280,6 +336,10 @@ function moduleValueChanged(value)
                                     }else{
                                         if(value.name=="rec"){
                                             local.sendNoteOn(1,parseInt(value.getParent().name.substring(5,6))-1,value.get());
+                                        }else{
+                                            if(value.name=="displayColor"){
+                                                updateScribleColor();
+                                            }
                                         }
                                     }
                                 }
@@ -341,95 +401,6 @@ function noteOnEvent(channel, pitch, velocity)
         var index = pitch-32;
         if(velocity==127) {local.values.strips.getChild('Strip '+(index+1)).push.set(1);}
     }
-    
-    //Is it a 'Encoder assign' button?
-    if (pitch >= 40 && pitch <= 45){
-        local.parameters.encodersAssign.setData(pitch-40);
-    }
-    
-    //Is it a 'Move' button?
-    if (pitch >= 46 && pitch <= 49){
-        if (pitch == 46) {local.parameters.bankIndex.set(local.parameters.bankIndex.get()-1);}
-        if (pitch == 47) {local.parameters.bankIndex.set(local.parameters.bankIndex.get()+1);}
-        if (pitch == 48) {local.parameters.stripIndex.set(local.parameters.stripIndex.get()-1);}
-        if (pitch == 49) {local.parameters.stripIndex.set(local.parameters.stripIndex.get()+1);}
-    }
-    
-    //Is it a 'Visual' button?
-    if (pitch >= 50 && pitch <= 53){
-        if (pitch == 50) {if(velocity==127){script.log("Flip");local.values.main.flip.set(1);}}
-        if (pitch == 51) {if(velocity==127){script.log("Global View");local.values.views.globalView.set(1);}}
-        if (pitch == 52) {if(velocity==127){script.log("Name/Value");local.values.display.name_Value.set(1);}}
-        if (pitch == 53) {if(velocity==127){script.log("SMPTE/Beats");local.values.display.SMPTE_Beats.set(1);}}
-    }
-    
-    //Is it a 'Function' button ?
-    if (pitch >= 54 && pitch <= 61){
-        var index = pitch-54;
-        if (velocity==127){script.log("Function"+ (index+1)); local.values.functionnal.getChild("f"+(index+1)).set(1);}
-    }
-    
-    //Is it a 'View' button ?
-    if (pitch >= 62 && pitch <= 69){
-        // Set new view value
-        local.parameters.activeView.setData(pitch-62);
-    }
-
-    //Is it a 'Modify' button ?
-    if (pitch >= 70 && pitch <= 73){
-        if (pitch == 70) {if(velocity==127){script.log("Shift");local.values.modify.shift.set(1);}}
-        if (pitch == 71) {if(velocity==127){script.log("Option");local.values.modify.option.set(1);}}
-        if (pitch == 72) {if(velocity==127){script.log("Control");local.values.modify.control.set(1);}}
-        if (pitch == 73) {if(velocity==127){script.log("Alt");local.values.modify.alt.set(1);}}
-    }
-
-    //Is it an 'Automation' button ?
-    if (pitch >= 74 && pitch <= 79){
-        if (pitch == 74) {if(velocity==127){script.log("Read/Off");local.values.automation.readOff.set(1);}}
-        if (pitch == 75) {if(velocity==127){script.log("Write");local.values.automation.write.set(1);}}
-        if (pitch == 76) {if(velocity==127){script.log("Trim");local.values.automation.trim.set(1);}}
-        if (pitch == 77) {if(velocity==127){script.log("Touch");local.values.automation.touch.set(1);}}
-        if (pitch == 78) {if(velocity==127){script.log("Latch");local.values.automation.latch.set(1);}}
-        if (pitch == 79) {if(velocity==127){script.log("Group");local.values.automation.group.set(1);}}
-    }
-
-    //Is it an 'Utility' button ?
-    if (pitch >= 80 && pitch <= 83){
-        if (pitch == 80) {if(velocity==127){script.log("Save");local.values.utility.save.set(1);}}
-        if (pitch == 81) {if(velocity==127){script.log("Undo");local.values.utility.undo.set(1);}}
-        if (pitch == 82) {if(velocity==127){script.log("Cancel");local.values.utility.cancel.set(1);}}
-        if (pitch == 83) {if(velocity==127){script.log("Enter");local.values.utility.enter.set(1);}}
-    }
-
-    //Is it a 'Transport' button?
-    if (pitch >= 84 && pitch <= 95){
-        if (pitch == 84) {if(velocity==127){script.log("Marker");local.values.transport.marker.set(1);}}
-        if (pitch == 85) {if(velocity==127){script.log("Nudge");local.values.transport.nudge.set(1);}}
-        if (pitch == 86) {if(velocity==127){script.log("Cycle");local.values.transport.cycle.set(1);}}
-        if (pitch == 87) {if(velocity==127){script.log("Drop");local.values.transport.drop.set(1);}}
-        if (pitch == 88) {if(velocity==127){script.log("Replace");local.values.transport.replace.set(1);}}
-        if (pitch == 89) {if(velocity==127){script.log("Click");local.values.transport.click.set(1);}}
-        if (pitch == 90) {if(velocity==127){script.log("Solo");local.values.transport.solo.set(1);}}
-        if (pitch == 91) {if(velocity==127){script.log("Rewind");local.values.transport.rewind.set(1);}}
-        if (pitch == 92) {if(velocity==127){script.log("Fast Forward");local.values.transport.forward.set(1);}}
-        if (pitch == 93) {if(velocity==127){script.log("Stop");local.values.transport.stop.set(1);}}
-        if (pitch == 94) {if(velocity==127){script.log("Play");local.values.transport.play.set(1);}}
-        if (pitch == 95) {if(velocity==127){script.log("RecSet");local.values.transport.recSet.set(1);}}
-    }
-
-    //Is it a 'Arrow' button?
-    if (pitch >= 96 && pitch <= 99){
-        if (pitch == 96) {if(velocity==127){script.log("Up");local.values.misc.up.set(1);}}
-        if (pitch == 97) {if(velocity==127){script.log("Down");local.values.misc.down.set(1);}}
-        if (pitch == 98) {if(velocity==127){script.log("Left");local.values.misc.left.set(1);}}
-        if (pitch == 99) {if(velocity==127){script.log("Right");local.values.misc.right.set(1);}}
-    }
-
-    //Is it a 'Misc' button?
-    if (pitch >= 100 && pitch <= 101){
-        if (pitch == 100) {if(velocity==127){script.log("Zoom");local.values.misc.zoom.set(1);}}
-        if (pitch == 101) {if(velocity==127){script.log("Scrub");local.values.misc.scrub.set(1);}}
-    }
 
     //Is it a fader touch?
     if (pitch >= 104 && pitch <= 111 && local.parameters.flashOnTouched.get()){
@@ -461,55 +432,6 @@ function noteOffEvent(channel, pitch, velocity)
     if (pitch >= 32 && pitch <= 39){
         var index = pitch-32;
         if(velocity==0) {local.values.strips.getChild('Strip '+(index+1)).push.set(0);}
-    }
-
-    //Is it a 'Visual' button?
-    if (pitch >= 50 && pitch <= 53){
-        if (pitch == 50) {if(velocity==0){local.values.main.flip.set(0);}}
-        if (pitch == 51) {if(velocity==0){local.values.views.globalView.set(0);}}
-        if (pitch == 52) {if(velocity==0){local.values.display.name_Value.set(0);}}
-        if (pitch == 53) {if(velocity==0){local.values.display.SMPTE_Beats.set(0);}}
-    }
-    //Is it a 'Function' button ?
-    if (pitch >= 54 && pitch <= 61){
-        var index = pitch-54;
-        if (velocity==0){script.log("Function"+ (index+1)); local.values.functionnal.getChild("f"+(index+1)).set(0);}
-    }
-
-    //Is it a button ?
-    if (pitch >= 70 && pitch <= 101){
-        if (pitch == 70) {if(velocity==0){local.values.modify.shift.set(0);}}
-        if (pitch == 71) {if(velocity==0){local.values.modify.option.set(0);}}
-        if (pitch == 72) {if(velocity==0){local.values.modify.control.set(0);}}
-        if (pitch == 73) {if(velocity==0){local.values.modify.alt.set(0);}}
-        if (pitch == 74) {if(velocity==0){local.values.automation.readOff.set(0);}}
-        if (pitch == 75) {if(velocity==0){local.values.automation.write.set(0);}}
-        if (pitch == 76) {if(velocity==0){local.values.automation.trim.set(0);}}
-        if (pitch == 77) {if(velocity==0){local.values.automation.touch.set(0);}}
-        if (pitch == 78) {if(velocity==0){local.values.automation.latch.set(0);}}
-        if (pitch == 79) {if(velocity==0){local.values.automation.group.set(0);}}
-        if (pitch == 80) {if(velocity==0){local.values.utility.save.set(0);}}
-        if (pitch == 81) {if(velocity==0){local.values.utility.undo.set(0);}}
-        if (pitch == 82) {if(velocity==0){local.values.utility.cancel.set(0);}}
-        if (pitch == 83) {if(velocity==0){local.values.utility.enter.set(0);}}
-        if (pitch == 84) {if(velocity==0){local.values.transport.marker.set(0);}}
-        if (pitch == 85) {if(velocity==0){local.values.transport.nudge.set(0);}}
-        if (pitch == 86) {if(velocity==0){local.values.transport.cycle.set(0);}}
-        if (pitch == 87) {if(velocity==0){local.values.transport.drop.set(0);}}
-        if (pitch == 88) {if(velocity==0){local.values.transport.replace.set(0);}}
-        if (pitch == 89) {if(velocity==0){local.values.transport.click.set(0);}}
-        if (pitch == 90) {if(velocity==0){local.values.transport.solo.set(0);}}
-        if (pitch == 91) {if(velocity==0){local.values.transport.rewind.set(0);}}
-        if (pitch == 92) {if(velocity==0){local.values.transport.forward.set(0);}}
-        if (pitch == 93) {if(velocity==0){local.values.transport.stop.set(0);}}
-        if (pitch == 94) {if(velocity==0){local.values.transport.play.set(0);}}
-        if (pitch == 95) {if(velocity==0){local.values.transport.recSet.set(0);}}
-        if (pitch == 96) {if(velocity==0){local.values.misc.up.set(0);}}
-        if (pitch == 97) {if(velocity==0){local.values.misc.down.set(0);}}
-        if (pitch == 98) {if(velocity==0){local.values.misc.left.set(0);}}
-        if (pitch == 99) {if(velocity==0){local.values.misc.right.set(0);}}
-        if (pitch == 100) {if(velocity==0){local.values.misc.zoom.set(0);}}
-        if (pitch == 101) {if(velocity==0){local.values.misc.scrub.set(0);}}
     }
 }
 
